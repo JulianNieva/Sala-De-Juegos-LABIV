3c5ce65a7259864d97a1b8c1b784c0ee3e02269e
JulianNieva <juliannieva90@gmail.com>
2023-09-10 19:38:54 -0300

    Iniciado proyecto y agregado componentes login,aboutme,home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule,
